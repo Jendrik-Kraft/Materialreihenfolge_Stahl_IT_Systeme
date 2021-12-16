1e01d107b94ab226872a56dba4449de03737225e
jenqhn <83419177+jenqhn@users.noreply.github.com>
2021-12-16 16:06:16 +0100

    Bsp. Datenbank als Excel und erste Einleseversuche
    2 Exceldatenbanken mit 50 Einträgen sowie das einlesen als dataframe

---
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
@@ -0,0 +1,5 @@
+import pandas as pd
+dummy_coils=pd.read_excel("./default_database_dummy_coils.xlsx")
+display(dummy_coils.head())
+coils=pd.read_excel(r"C:\Users\jenny\PycharmProjects\pythonProject\StehlingStahl\GIT\MATERIALREIHENFOLGE_STAHL\REIHENFOLGE_APP\default_database1.xlsx")
+display(coils.head())
