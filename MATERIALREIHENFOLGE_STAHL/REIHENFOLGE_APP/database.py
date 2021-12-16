30109f02d7732a72a3d45807dab25e81342def57
jenqhn <83419177+jenqhn@users.noreply.github.com>
2021-12-16 17:10:33 +0100

    Converted to function
    Function "get_coils_from_database" is created using pandas and openpyxl

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
@@ -1,5 +1,9 @@
 import pandas as pd
-dummy_coils=pd.read_excel("./default_database_dummy_coils.xlsx")
-display(dummy_coils.head())
-coils=pd.read_excel(r"C:\Users\jenny\PycharmProjects\pythonProject\StehlingStahl\GIT\MATERIALREIHENFOLGE_STAHL\REIHENFOLGE_APP\default_database1.xlsx")
-display(coils.head())
+def get_coils_from_database():
+    dummy_coils=pd.read_excel("./default_database_dummy_coils.xlsx")
+    print(dummy_coils.head())
+    coils=pd.read_excel("./default_database1.xlsx")
+    #print(coils["Hight"][5])
+
+
+get_coils_from_database()
