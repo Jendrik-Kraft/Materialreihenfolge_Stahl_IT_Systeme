6240746a999be8af6ceb4f035028ac465fed4c72
Jendrik-Kraft <83779920+Jendrik-Kraft@users.noreply.github.com>
2021-12-17 14:38:12 +0100

    Updated doc string, included return value
    Updated doc string, included return value

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
@@ -1,6 +1,10 @@
 import pandas as pd
 def get_coils_from_database():
+    """
+    This function is used to get the databse
+    :return: List of two panda dataframes, one for coils and one for dummy coils
+    """
     dummy_coils=pd.read_excel("./default_database_dummy_coils.xlsx")
     print(dummy_coils.head())
     coils=pd.read_excel("./default_database1.xlsx")
-    #print(coils["Hight"][5])
+    return [coils, dummy_coils]
