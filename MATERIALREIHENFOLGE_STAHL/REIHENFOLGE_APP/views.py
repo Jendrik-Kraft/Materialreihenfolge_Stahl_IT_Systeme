224d1841c87fe52f6a69fe5afd882075bdd42570
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2021-12-17 16:05:44 +0100

    Added first try of right format for HTML

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 # Create your views here.
 import random
-
+from .database import *
+from .utility import *
 def hi(request):
-
-    return render(request, 'REIHENFOLGE/graph_try.html', {"name": random.randint(0,100), "number": random.randint(0,1100)})
+    [coils, dummy_coils] = get_coils_from_database()
+    coils_list = coils.values.tolist()
+    liste_for_js = []
+    for coil in coils_list:
+        liste_for_js.append({"x":coil[1], "y":coil[2]})
+    print(liste_for_js)
+    fitting_coils = find_fusible_coils(coils, 10, 50)
+    return render(request, 'REIHENFOLGE/graph_try.html', {"name": liste_for_js, "number": random.randint(0,1100)})
