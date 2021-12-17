9e6ad009ded3a2a3539a78cab60338bb93bedfb0
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2021-12-17 15:05:37 +0100

    Fixed flake8 issues with empty lines and missing whitespaces

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
@@ -1,9 +1,11 @@
 import pandas as pd
+
+
 def get_coils_from_database():
     """
     This function is used to get the databse
     :return: List of two panda dataframes, one for coils and one for dummy coils
     """
-    dummy_coils=pd.read_excel("./default_database_dummy_coils.xlsx")
-    coils=pd.read_excel("./default_database1.xlsx")
+    dummy_coils = pd.read_excel("./default_database_dummy_coils.xlsx")
+    coils = pd.read_excel("./default_database1.xlsx")
     return [coils, dummy_coils]
