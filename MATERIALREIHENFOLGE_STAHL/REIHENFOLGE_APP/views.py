12c0fa24d270e90e52b5a9e59458ef4504350c7f
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2021-12-17 14:41:54 +0100

    Functionality to find all fusible coils with a given tolerance for width and height

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 import random
 
 def hi(request):
+
     return render(request, 'REIHENFOLGE/graph_try.html', {"name": random.randint(0,100), "number": random.randint(0,1100)})
