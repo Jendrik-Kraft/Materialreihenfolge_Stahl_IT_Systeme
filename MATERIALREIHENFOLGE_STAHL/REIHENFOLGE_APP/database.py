3096a7be27a79c55be8c778f5cd5ef0bcb1c2c31
Jendrik-Kraft <83779920+Jendrik-Kraft@users.noreply.github.com>
2021-12-20 16:09:05 +0100

    Anpassen der datenstruktur (#3)
    * Bsp. Datenbank als Excel und erste Einleseversuche
    
    2 Exceldatenbanken mit 50 Einträgen sowie das einlesen als dataframe
    
    * Converted to function
    
    Function "get_coils_from_database" is created using pandas and openpyxl
    
    * Update database.py
    
    Deleted function call for master merge
    
    * Updated doc string, included return value
    
    Updated doc string, included return value
    
    * Functionality to find all fusible coils with a given tolerance for width and height
    
    * Fixed typo from value to values, included database function, testes functionality
    
    * Deleted print
    
    Deleted print
    
    * Added missing RecordID of Line 50
    
    * Fixed flake8 issues with empty lines and missing whitespaces
    
    * deleted print statement
    
    * Functionality to find all fusible coils with a given tolerance for width and height
    
    * Fixed typo from value to values, included database function, testes functionality
    
    * deleted print statement
    
    * Removed .pyc files
    
    * all pyc files removed
    
    * Added first try of right format for HTML
    
    * Moved functionality to functions
    implemented them in the views funtion as an example - not used yet
    
    Co-authored-by: jenqhn <83419177+jenqhn@users.noreply.github.com>

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/database.py
@@ -6,6 +6,6 @@ def get_coils_from_database():
     This function is used to get the databse
     :return: List of two panda dataframes, one for coils and one for dummy coils
     """
-    dummy_coils = pd.read_excel("./default_database_dummy_coils.xlsx")
-    coils = pd.read_excel("./default_database1.xlsx")
+    dummy_coils = pd.read_excel("./REIHENFOLGE_APP/default_database_dummy_coils.xlsx")
+    coils = pd.read_excel("./REIHENFOLGE_APP/default_database1.xlsx")
     return [coils, dummy_coils]
--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -1,7 +1,16 @@
+import json
+
 from django.shortcuts import render
 # Create your views here.
 import random
+from .database import *
+from .utility import *
+
 
 def hi(request):
+    [coils, dummy_coils] = get_coils_from_database()
+    coil_list_for_js = build_coil_list_for_js(coils)
+    fitting_coils = find_fusible_coils(coils, 30, 100)  # TODO Use parameters from URL to configure tolerances
+    fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
+    return render(request, 'REIHENFOLGE/graph_try.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js})
 
-    return render(request, 'REIHENFOLGE/graph_try.html', {"name": random.randint(0,100), "number": random.randint(0,1100)})
