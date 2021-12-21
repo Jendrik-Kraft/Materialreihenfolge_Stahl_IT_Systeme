4d1523482ed8272e6caf10b21d07acc784079c38
Jendrik-Kraft <83779920+Jendrik-Kraft@users.noreply.github.com>
2021-12-21 15:47:11 +0100

    Einbinden von HTML und JS in main project (#4)
    * Included html and js, changed view to right file default 0/0
    
    * added script.js
    
    * added storage in var
    
    Co-authored-by: Marcel3197 <mseemann@seetac.de>

--- MATERIALREIHENFOLGE_STAHL/MATERIALREIHENFOLGE_STAHL/settings.py
+++ MATERIALREIHENFOLGE_STAHL/MATERIALREIHENFOLGE_STAHL/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MUSIC_PLAYER_DIR = BASE_DIR / 'REIHENFOLGE_APP'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    MUSIC_PLAYER_DIR / 'static',
+]
 
 
 # Quick-start development settings - unsuitable for production
--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -7,10 +7,9 @@ from .database import *
 from .utility import *
 
 
-def hi(request):
+def hi(request, dicke=0, breite=0):
     [coils, dummy_coils] = get_coils_from_database()
     coil_list_for_js = build_coil_list_for_js(coils)
-    fitting_coils = find_fusible_coils(coils, 30, 100)  # TODO Use parameters from URL to configure tolerances
+    fitting_coils = find_fusible_coils(coils, breite, dicke)
     fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
-    return render(request, 'REIHENFOLGE/graph_try.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js})
-
+    return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js})
