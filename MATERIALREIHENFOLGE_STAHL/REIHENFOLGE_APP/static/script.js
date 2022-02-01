0bb64c05a2ae1d3be94f70b9bdde8eab5b6c56ef
bport734 <97114531+bport734@users.noreply.github.com>
2022-02-01 19:13:07 +0100

    Update script.js

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/script.js
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/script.js
@@ -15,7 +15,7 @@ if (!firstTime) {
 function updateToleranz() {
   var tol_dicke = document.getElementById("input_dicke").value;
   var tol_breite = document.getElementById("input_breite").value;
-  if((tol_dicke.length == 0) || (tol_breite.length == 0)){
+  if((typeof(tol_dicke) != 'undefined' && tol_dicke != null) || (typeof(tol_breite) != 'undefined' && tol_breite != null)){
     tol_dicke = "0";
     tol_breite = "0";
   }
