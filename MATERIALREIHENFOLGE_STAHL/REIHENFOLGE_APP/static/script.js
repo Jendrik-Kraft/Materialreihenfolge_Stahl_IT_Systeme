9324d9c89930ac0530a458b306e606027c448004
bport734 <97114531+bport734@users.noreply.github.com>
2022-02-02 16:05:23 +0100

    Update script.js

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/script.js
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/script.js
@@ -16,9 +16,9 @@ function updateToleranz() {
   var tol_dicke = document.getElementById("input_dicke").value;
   var tol_breite = document.getElementById("input_breite").value;
   //if((typeof(tol_dicke) != 'undefined' && tol_dicke != null && tol_dicke != "") || (typeof(tol_breite) != 'undefined' && tol_breite != null && tol_breite != "")){
-  if(tol_dicke && tol_breite){
-    localStorage.setItem("Breite", tol_breite);
-    localStorage.setItem("Dicke", tol_dicke);
+  if(tol_dicke != null && tol_breite != null){
+    localStorage.setItem("Breite1", tol_breite);
+    localStorage.setItem("Dicke1", tol_dicke);
     location.href =
     "http://127.0.0.1:8000/display_graph/" + tol_breite + "15/50" + tol_dicke;
   }
