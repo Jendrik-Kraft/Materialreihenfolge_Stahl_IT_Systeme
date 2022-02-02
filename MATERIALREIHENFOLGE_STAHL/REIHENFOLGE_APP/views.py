9173af59615b04a0e6206c6f39d4946397848934
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2022-02-02 16:37:14 +0100

    Change to use fitting path instead of all path'

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -17,10 +17,11 @@ def hi(request, dicke=0, breite=0, pfade="all"):
         dummy_coils_for_js = build_dummy_coil_list_for_js(complete_coils)
     else:
         fitting_coils = find_fusible_coils(coils, breite, dicke)
+        complete_coils = coils
 
     coil_list_for_js = build_coil_list_for_js(coils)
     fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
     print(complete_coils)
     path = build_path_for_js(complete_coils)
-    return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": path,
+    return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js,
                                                       "dummy_coils": dummy_coils_for_js})
