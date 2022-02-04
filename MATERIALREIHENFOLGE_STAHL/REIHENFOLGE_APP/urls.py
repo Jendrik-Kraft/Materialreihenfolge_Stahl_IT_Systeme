1e2965b8e494f2e54a534564ca6aca8db3325702
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2022-02-04 18:21:19 +0100

    Fixed reloading bug, implemented favicon

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/urls.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/urls.py
@@ -19,4 +19,5 @@ from . import views
 urlpatterns = [
     path('', views.hi, name="home-page"),
     path('display_graph/<int:dicke>/<int:breite>/<str:pfade>', views.hi, name="home-page"),
+    path('display_graph/<int:dicke>/<int:breite>', views.hi, name="home-page"),
 ]
