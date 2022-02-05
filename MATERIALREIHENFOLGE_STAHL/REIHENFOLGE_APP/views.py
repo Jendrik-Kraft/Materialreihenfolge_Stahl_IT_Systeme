bd23f843e628ac6dff07e2a62631e684ff02a2d1
Jendrik Kraft <jendrik.diekrafts2@yahoo.de>
2022-02-05 14:38:38 +0100

    trying to fix index error

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -7,7 +7,7 @@ from .database import *
 from .utility import *
 
 
-def hi(request, dicke=0, breite=0, pfade="all"):
+def load_page(request, dicke=0, breite=0, pfade="all"):
     dummy_coils_for_js = []
     [coils, dummy_coils] = get_coils_from_database()
     # if both tolerances are 0, generate_path will error. Thus we only execute it if tolerances are given
@@ -20,8 +20,41 @@ def hi(request, dicke=0, breite=0, pfade="all"):
         complete_coils = coils
 
     coil_list_for_js = build_coil_list_for_js(coils)
-    fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
+
     print(complete_coils)
-    path = build_path_for_js(complete_coils)
+
+    if pfade == "all":
+        fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
+    else:
+        fitting_coils_for_js = build_path_for_js(complete_coils)
     return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js,
                                                       "dummy_coils": dummy_coils_for_js})
+
+#import json
+
+# from django.shortcuts import render
+# # Create your views here.
+# import random
+# from .database import *
+# from .utility import *
+#
+#
+# def load_page(request, dicke=0, breite=0, pfade="all"):
+#     dummy_coils_for_js = []
+#     [coils, dummy_coils] = get_coils_from_database()
+#
+#     if pfade == "all":
+#         fitting_coils = find_fusible_coils(coils, breite, dicke)
+#         complete_coils = coils
+#         fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
+#
+#     # if both tolerances are 0, generate_path will error. Thus we only execute it if tolerances are given
+#     if pfade == "best_path" and (dicke != 0 or breite != 0):
+#         complete_coils = generate_path(coils, dummy_coils, breite, dicke)
+#         #fitting_coils = find_fusible_coils(coils, breite, dicke)
+#         dummy_coils_for_js = build_dummy_coil_list_for_js(complete_coils)
+#         fitting_coils_for_js = build_path_for_js(complete_coils)
+#
+#     coil_list_for_js = build_coil_list_for_js(coils)
+#     return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js,
+#                                                       "dummy_coils": dummy_coils_for_js})
