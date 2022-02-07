09effbfdd8835e8f7048cd735122d8d4991bff90
KaiB1904 <94189170+KaiB1904@users.noreply.github.com>
2022-02-07 22:00:50 +0100

    Round after two decimals

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -9,6 +9,7 @@ from .utility import *
 
 def mainpage(request, dicke=0, breite=0, pfade="all"):
     [coils, dummy_coils] = get_coils_from_database()
+    coils = round(coils, 2)
     coil_list_for_js = build_coil_list_for_js(coils)
     fitting_coils = find_fusible_coils(coils, breite, dicke)
     fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
