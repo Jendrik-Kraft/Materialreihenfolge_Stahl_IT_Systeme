cce81c17912a1310c12eda653f7f55ccfc945a7d
KaiB1904 <94189170+KaiB1904@users.noreply.github.com>
2022-02-10 20:04:57 +0100

    Save Button

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/editPageScript.js
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/static/editPageScript.js
@@ -5,37 +5,44 @@ function onChangeToMainPageButtonClick() {
     "http://127.0.0.1:8000/display_graph/10/10";
 }
 
+function onSaveChangesButtonClick() {
+    var coilsTableData = document.querySelector("tbody.coilsTable-Body");
+    var tableId = document.getElementById('coilsTable');
+    var tBody = tableId.getElementsByTagName('tbody')[0];
+    var tableRow = tBody.getElementsByTagName('tr');
+}
+
 function buildTable() {
-      var scoreDiv = document.querySelector("div.scoreboard"); // Find the scoreboard div in our html
+      var coilDiv = document.querySelector("div.coilTable"); // Find the scoreboard div in our html
       //scoreDiv.style.background = "green";
       var tableHeaders = ["ID","Width", "Height"];
-      const createScoreboardTable = () =>
+      const createCoilTable = () =>
       {
-          while (scoreDiv.firstChild){ scoreDiv.removeChild(scoreDiv.firstChild); }// Remove all children from scoreboard div (if any)
-          let scoreboardTable = document.createElement('table'); // Create the table itself
-          scoreboardTable.className = 'scoreboardTable';
-          let scoreboardTableHead = document.createElement('thead') ;// Creates the table header group element
-          scoreboardTableHead.className = 'scoreboardTableHead';
-          let scoreboardTableHeaderRow = document.createElement('tr');// Creates the row that will contain the headers
-          scoreboardTableHeaderRow.className = 'scoreboardTableHeaderRow';
+          while (coilDiv.firstChild){ coilDiv.removeChild(coilDiv.firstChild); }// Remove all children from scoreboard div (if any)
+          let coilsTable = document.createElement('table'); // Create the table itself
+          coilsTable.className = 'coilsTable';
+          let coilsTableHead = document.createElement('thead') ;// Creates the table header group element
+          coilsTableHead.className = 'coilsTableHead';
+          let coilsTableHeaderRow = document.createElement('tr');// Creates the row that will contain the headers
+          coilsTableHeaderRow.className = 'coilsTableHeaderRow';
           // Will iterate over all the strings in the tableHeader array and will append the header cells to the table header row
           tableHeaders.forEach(header => {
-              let scoreHeader = document.createElement('th'); // Creates the current header cell during a specific iteration
-              scoreHeader.innerText = header;
-              scoreboardTableHeaderRow.append(scoreHeader); // Appends the current header cell to the header row
+              let coilsHeader = document.createElement('th'); // Creates the current header cell during a specific iteration
+              coilsHeader.innerText = header;
+              coilsTableHeaderRow.append(coilsHeader); // Appends the current header cell to the header row
               })
-          scoreboardTableHead.append(scoreboardTableHeaderRow); // Appends the header row to the table header group element
-          scoreboardTable.append(scoreboardTableHead);
-          let scoreboardTableBody = document.createElement('tbody');// Creates the table body group element
-          scoreboardTableBody.className = "scoreboardTable-Body";
-          scoreboardTable.append(scoreboardTableBody); // Appends the table body group element to the table
-          scoreDiv.append(scoreboardTable); // Appends the table to the scoreboard div
+          coilsTableHead.append(coilsTableHeaderRow); // Appends the header row to the table header group element
+          coilsTable.append(coilsTableHead);
+          let coilsTableBody = document.createElement('tbody');// Creates the table body group element
+          coilsTableBody.className = "coilsTable-Body";
+          coilsTable.append(coilsTableBody); // Appends the table body group element to the table
+          coilDiv.append(coilsTable); // Appends the table to the scoreboard div
       }
 
-      const appendScores = (coil) => {
-          const scoreboardTable = document.querySelector('.scoreboardTable'); // Find the table we created
-          let scoreboardTableBodyRow = document.createElement('tr'); // Create the current table row
-          scoreboardTableBodyRow.className = 'scoreboardTableBodyRow';
+      const appendCoils = (coil) => {
+          const coilsTable = document.querySelector('.coilsTable'); // Find the table we created
+          let coilsTableBodyRow = document.createElement('tr'); // Create the current table row
+          coilsTableBodyRow.className = 'coilsTableBodyRow';
           // Lines 72-85 create the 5 column cells that will be appended to the current table row
           let coilID = document.createElement('td');
           coilID.innerText = coil[0];
@@ -43,17 +50,20 @@ function buildTable() {
           coilWidth.innerText = coil[2];
           let coilHight = document.createElement('td');
           coilHight.innerText = coil[1];
-          scoreboardTableBodyRow.append(coilID, coilWidth, coilHight);// Append all 5 cells to the table row
-          scoreboardTable.append(scoreboardTableBodyRow);// Append the current row to the scoreboard table body
+          coilsTableBodyRow.append(coilID, coilWidth, coilHight);// Append all 5 cells to the table row
+          coilsTable.append(coilsTableBodyRow);// Append the current row to the scoreboard table body
           }
       //var coils = {{ coils|safe }} ;
       //var coils=coils;
-      createScoreboardTable();// Clears scoreboard div if it has any children nodes, creates & appends the table
+      createCoilTable();// Clears scoreboard div if it has any children nodes, creates & appends the table
+      coilDiv.setAttribute("contenteditable", true);
+      var coilsTableHead = document.querySelector("thead.coilsTableHead");
+      coilsTableHead.setAttribute("contenteditable", false);
       // Iterates through all the objects in the scores array and appends each one to the table body
 
       for (const coil of coils) {
           var coilIndex = coils.indexOf(coil) + 1;// Index of score in score array for global ranking (these are already sorted in the back-end)
-          appendScores(coil); // Creates and appends each row to the table body
+          appendCoils(coil); // Creates and appends each row to the table body
           }
 
 }
