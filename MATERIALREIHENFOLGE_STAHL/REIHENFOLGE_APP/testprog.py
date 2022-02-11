c88c47f1016ed50bc23e6e75b0eea14b56763ddd
Patrick Großheim <patrick.grossheim@gmail.com>
2022-02-11 14:22:28 +0100

    Added error Handling and looked up Database error

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/testprog.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/testprog.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 import random
 #from database import *
 from utility import *
+import pyodbc
+import numpy as np
 
 def find_fusible_coils(coils, tolerance_h_per, tolerance_w_abs):
     """
@@ -53,6 +55,55 @@ def get_coils_from_database():
     coils = pd.read_excel("default_database_statictest.xlsx")
     return [coils, dummy_coils]
 
+# test MS Access Drivers availability for Python
+# msa_drivers = [x for x in pyodbc.drivers() if 'ACCESS' in x.upper()]
+# print(f'MS-Access Drivers : {msa_drivers}')
+# make sure, that "Microsoft Access Driver (*.mdb, *.accdb)" is part of the drivers
+
+def get_coils_from_database_new():
+    """
+    This function is used to get the databse from MS Access
+    :return: List of two panda dataframes, one for coils and one for dummy coils
+    """
+
+    # Establish Database Connection in MS Access
+    # CoilsDatabase includes default data and dummy coils
+    try:
+        con_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Patrick\Documents\Schule\Master\Kurse\IT-Systeme und IT-Projektmanagement\Projekt\Code\Materialreihenfolge_Stahl_IT_Systeme\MATERIALREIHENFOLGE_STAHL\REIHENFOLGE_APP\CoilsDatabase.accdb;'
+        conn = pyodbc.connect(con_string)
+        print("Connected To Database")
+
+    except pyodbc.Error as e:
+        print("Error in Connection", e)
+
+    # Selecting Data from MS Access Database
+    try:
+        cursor = conn.cursor()
+
+        # load default data:
+        cursor.execute('SELECT * FROM Coils_default')       # "Coils_default" is the table with the default data
+        coils_list = cursor.fetchall()                      # data is pulled as list
+        coils_array = np.array(coils_list)                  # convert list to array
+        get_columns = [column[0] for column in cursor.description]  # collects columnnames
+        # now the array can be nicely written into DataFrame including the columnnames
+        coils = pd.DataFrame(data=coils_array, columns=get_columns)
+        #print(coils.head())
+
+        # load dummy coils:
+        cursor.execute('SELECT * FROM dummy_coils')         # "dummy_coils" is the table with the dummy coil data
+        dummy_coils_list = cursor.fetchall()                # data is pulled as list
+        dummy_coils_array = np.array(dummy_coils_list)      # convert list to array
+        get_dummy_columns = [column[0] for column in cursor.description] # collects columnnames
+        dummy_coils = pd.DataFrame(data=dummy_coils_array, columns=get_dummy_columns)   # array to DataFrame
+        # print(dummy_coils.head())
+
+    except pyodbc.Error as e:
+        print("Error in Connection")
+        coils = None
+        dummy_coils = None
+
+    return [coils, dummy_coils]
+
 def generate_path(coils, dummy_coils, tolerance_h_per, tolerance_w_abs):
     """
     This function compares the number of dummy coils for different path-finding algorithms and returns the complete coil
@@ -75,10 +126,20 @@ def generate_path(coils, dummy_coils, tolerance_h_per, tolerance_w_abs):
     coils_path2 = coils.sort_values(by=['Width','Hight'], ignore_index=True)
     complete_coil_list_path2 = get_complete_coil_list(coils_path2, dummy_coils, tolerance_h_per, tolerance_w_abs)
 
-    if(len(complete_coil_list_path1[complete_coil_list_path1['dummy'] == True]) < len(complete_coil_list_path2[complete_coil_list_path2['dummy'] == True])):
-        return complete_coil_list_path1
-    else:
-        return complete_coil_list_path2
+    # Checke ob Pfade möglich sind
+    if not isinstance(coils_path1, pd.DataFrame): # Pfad 1 nicht möglich
+        if isinstance(coils_path2, pd.DataFrame): # Pfad 1 nicht möglich, Pfad 2 möglich
+            return complete_coil_list_path2
+        else:   # Pfad 1 und Pfad 2 nicht möglich
+            return -1
+    else:   # Pfad 1 möglich
+        if not isinstance(coils_path2, pd.DataFrame):   # Pfad 2 nicht möglich
+            return complete_coil_list_path1
+        else:   # Pfad 1 und Pfad 2 möglich: Verwende Pfad mit weniger Dummy-Coils
+            if(len(complete_coil_list_path1[complete_coil_list_path1['dummy'] == True]) < len(complete_coil_list_path2[complete_coil_list_path2['dummy'] == True])):
+                return complete_coil_list_path1
+            else:
+                return complete_coil_list_path2
 
 def get_complete_coil_list(coils, dummy_coils, tolerance_h_per, tolerance_w_abs):
     """
@@ -90,7 +151,6 @@ def get_complete_coil_list(coils, dummy_coils, tolerance_h_per, tolerance_w_abs)
     :param tolerance_w_abs:
     :return:
     """
-    # ToDo: Raise Error when it's not possible to create a fusible coil-list
 
     # Add additional rows to coils pandas dataframe
     # tolerance_h, h_min, h_max
@@ -122,7 +182,10 @@ def get_complete_coil_list(coils, dummy_coils, tolerance_h_per, tolerance_w_abs)
             complete_coils = complete_coils.append(coils.iloc[n+1], ignore_index=True)
         # Nächstes Coil liegt nicht im Toleranzbereich: Dummycoil(s) notwendig
         else:
-            complete_coils = complete_coils.append(get_dummy_coils(coils.iloc[[n]],coils.iloc[[n+1]], dummy_coils))
+            dummy_coils_selection = get_dummy_coils(coils.iloc[[n]],coils.iloc[[n+1]], dummy_coils)
+            if not isinstance(dummy_coils_selection, pd.DataFrame):
+                return -1
+            complete_coils = complete_coils.append(dummy_coils_selection)
             complete_coils = complete_coils.append(coils.iloc[n+1], ignore_index=True)
 
     # Clean not needed parameters from pandas dataframe
@@ -137,7 +200,7 @@ def get_dummy_coils(coil1, coil2, dummy_coils):
     :param dummy_coils:
     :return:
     """
-    # ToDo: Raise Error when it's not possible to fuse 2 coils with given dummy-coils
+
     dummy_coils_rslt = dummy_coils
     # Check hight:
     # Hight von coil2 im Toleranzbereich von coil1
@@ -183,14 +246,21 @@ def get_dummy_coils(coil1, coil2, dummy_coils):
         else:
             dummy_coils_rslt = dummy_coils_rslt.sort_values(by=['Hight','Width'], ascending =[False, False], ignore_index=True)
 
-    # Wähle erstes Dummy-Coil (alle Coils aus Liste passen, aber das erste stellt das sinnvolste Coil dar)
+    # Wähle erstes Dummy-Coil (alle Coils aus Liste passen, aber das erste stellt das sinnvollste Coil dar)
     dummy_coils_rslt = dummy_coils_rslt.iloc[[0]]
+    # Neues Dummycoil entspricht dem vorherigen Dummycoil -> Keine passenden Dummycoils zur Überbrückung vorhanden
+    if coil1.Hight.iloc[0] == dummy_coils_rslt.Hight.iloc[0] & coil1.Hight.iloc[0] == dummy_coils_rslt.Hight.iloc[0] & coil1.dummy.iloc[0] == True:
+            return -1
     # Coil2 (End-Coil) im Toleranzbereich vom Dummy-Coil
     if (dummy_coils_rslt.tolerance_h_top.iloc[0] >= coil2.Hight.iloc[0] >= dummy_coils_rslt.tolerance_h_bottom.iloc[0]) and (dummy_coils_rslt.tolerance_w_top.iloc[0] >= coil2.Width.iloc[0] >= dummy_coils_rslt.tolerance_w_bottom.iloc[0]):
         return dummy_coils_rslt
     # Coil2 (End-Coil) noch immer nicht im Toleranzbereiche vom ausgewählten Dummy-Coil
     else:
-        dummy_coils_rslt = dummy_coils_rslt.append(get_dummy_coils(dummy_coils_rslt, coil2, dummy_coils), ignore_index=True)
+        dummy_next = get_dummy_coils(dummy_coils_rslt, coil2, dummy_coils)
+        if not isinstance(dummy_next, pd.DataFrame):
+            return -1
+        else:
+            dummy_coils_rslt = dummy_coils_rslt.append(dummy_next, ignore_index=True)
 
     return dummy_coils_rslt
 
@@ -198,6 +268,11 @@ def get_dummy_coils(coil1, coil2, dummy_coils):
 
 
 [coils, dummy_coils] = get_coils_from_database()
+#[coilsnew, dummy_coilsnew] = get_coils_from_database_new()
+
+
+
+
 tolerance_h_per = 10
 tolerance_w_abs = 10
 print('-----------------------------------------------------')
@@ -213,5 +288,19 @@ print(complete_coils[complete_coils['dummy'] == False])
 print('-----------------------------------------------------')
 print('Gesamt-Coilliste für gewählten Pfad:')
 print(complete_coils)
+
+#print('\n\n\n\n-----------------------------------------------------')
+#print('Eingelesene Coils:')
+#print(coilsnew)
+#print('-----------------------------------------------------')
+#print('Eingelesene Dummy-Coils:')
+#print(dummy_coilsnew)
+#print('-----------------------------------------------------')
+#complete_coilsnew = generate_path(coilsnew, dummy_coilsnew, tolerance_h_per, tolerance_w_abs)
+#print('Erstellter Pfad:')
+#print(complete_coilsnew[complete_coilsnew['dummy'] == False])
+#print('-----------------------------------------------------')
+#print('Gesamt-Coilliste für gewählten Pfad:')
+#print(complete_coilsnew)
 
 
