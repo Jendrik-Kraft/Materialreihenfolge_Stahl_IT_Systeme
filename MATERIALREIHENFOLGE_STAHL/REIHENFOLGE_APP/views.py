99700a4f4961a14f80e2950fd29efef521dd1719
Marcel3197 <mseemann@seetac.de>
2022-02-16 19:31:53 +0100

    Bug fix

--- MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
+++ MATERIALREIHENFOLGE_STAHL/REIHENFOLGE_APP/views.py
@@ -9,7 +9,7 @@ def mainpage(request, dicke=0, breite=0, pfade="all"):
     print(breite)
     dummy_coils_for_js = []
     [coils, dummy_coils] = get_coils_from_database()
-    # error = 0
+    error = 0
     if pfade == "all":
         fitting_coils = find_fusible_coils(coils, breite, dicke)
         fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
@@ -21,7 +21,7 @@ def mainpage(request, dicke=0, breite=0, pfade="all"):
         if type(complete_coils) is int:  # Rückgabe -1 -> es wurde kein Pfad gefunden
             fitting_coils = find_fusible_coils(coils, 0, 0)
             fitting_coils_for_js = build_fitting_coils_list_for_js(coils, fitting_coils)
-            #error = 1
+            error = 1
             #dummy_coils_for_js = build_coil_list_for_js(dummy_coils)
         else:
             #fitting_coils = find_fusible_coils(coils, breite, dicke)
@@ -29,7 +29,7 @@ def mainpage(request, dicke=0, breite=0, pfade="all"):
             fitting_coils_for_js = build_path_for_js(complete_coils)
     coil_list_for_js = build_coil_list_for_js(coils)
     return render(request, 'REIHENFOLGE/index.html', {"coils": coil_list_for_js, "connections": fitting_coils_for_js,
-                                                      "dummy_coils": dummy_coils_for_js})  # "error": error
+                                                      "dummy_coils": dummy_coils_for_js, "error": error })
 
 
 def editpage(request, table=""):
